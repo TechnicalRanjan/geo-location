d3a1b00c93f9046f849efc844615dd6061ccd4fb
Priyesh Ranjan <priyesh.ranjan@teamlease.com>
2023-01-11 20:51:11 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import {Button, Space, message,DatePicker} from 'antd'
+import { useEffect, useState } from 'react'
+import './App.css'
 
-function App() {
+const App = () => {
+
+  const [location, setLocation]=useState({
+    loaded:false,
+    coordinates:{lat:"",lang:""}
+  })
+  const onSuccess =(location) => {
+    console.log(location);
+    setLocation({
+      loaded:true,
+      coordinates:{
+        lat:location.coords.latitude,
+        lang:location.coords.longitude,
+      }
+      
+    })
+    setData(`Your latitude: ${location.coords.latitude} \n\n\n Your longitude:${location.coords.longitude}`)
+  }
+  const onError =(error)=>{
+    setLocation({
+      loaded:true,
+      error,
+    })
+  }
+  
+    useEffect(() => {
+      if(!("geolocation" in navigator)){
+        onError({
+          code:0,
+          message:"Geolocation not supported"
+        })
+      }
+     setData()
+    },[])
+  
+  const [data, setData] = useState('')
+
+  const handleChange = (value) => {
+    navigator.geolocation.getCurrentPosition(onSuccess, onError)
+
+    if(location.loaded){
+      return message.success(`Your latitude: ${location.coordinates.lat} \n\n\n Your longitude:${location.coordinates.lang}`);
+    }
+    message.error(`location not loaded yet`)
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='location-container'>
+      <Button  type='primary' onClick={handleChange}>Get Geo Location</Button>
+      <Space>
+
+      <div className='message-box'>
+      {
+      
+      location.loaded &&
+      <p>
+        {data}
+      </p>
+      }
+      </div>
+      </Space>
+
     </div>
   );
-}
+};
 
 export default App;
+
